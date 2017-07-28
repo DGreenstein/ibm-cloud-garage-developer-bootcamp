5973af781644f0233356903bfca0663471e7971b
D Greenstein <david.h.greenstein@gmail.com>
2017-07-28 15:18:25 -0500

    make test for both positive and negative test of palindrome.

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,20 +3,22 @@ describe.only('the palindrome canary spec', () => {
     true.should.be.true();
   });
 
-  function isPalindrome() {
-    return true;
+  function isPalindrome(word) {
+    return word === word.split('').reverse().join('');
   }
 
   describe('a palindrome', () => {
     it('is racecar', () => {
       isPalindrome('racecar').should.be.true();
     });
-    
-    it('is not racecars');
-    it('is not "race car"');
-    it('is 12321');
-    it('is not 54321');
-    it('"   " is not a palindrome');
+    it('is not racecars', () => {
+      isPalindrome('racecars').should.be.false();
+    });
+    it('is not "race car"', () => {
+      isPalindrome('race car').should.be.false();
+    });
+    it('"   " is not a palindrome', () => {
+    });
     it('"" is not a palindrome');
     it('is "mom dad mom"');
   });
