bfaf8e026ed43bb3736ca08d1215f48c29819588
D Greenstein <david.h.greenstein@gmail.com>
2017-07-31 15:26:29 -0500

    make primeFactorsOf function aware of 1 and 2

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,12 +1,18 @@
-const primeFactorsOf = () => {
-  return [];
+const primeFactorsOf = (number) => {
+  const factors = [];
+  if (number > 1) {
+    factors.push(2);
+  }
+  return factors;
 };
 
 describe.only('prime factors', () => {
   it('return [none] for 1', () => {
     primeFactorsOf(1).should.deepEqual([]);
   });
-  it('return [2] for 2');
+  it('return [2] for 2', () => {
+    primeFactorsOf(2).should.deepEqual([2]);
+  });
   it('return [3] for 3');
   it('return [2, 2] for 4');
   it('return [5] for 5');
