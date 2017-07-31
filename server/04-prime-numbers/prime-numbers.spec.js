9f81cc4bd20d959c60356771b0ca3311b127f1b8
D Greenstein <david.h.greenstein@gmail.com>
2017-07-31 16:33:05 -0500

    calculate primeFactorsOf 2016 reliably.

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,23 +1,53 @@
+/* eslint-disable no-param-reassign */
 const primeFactorsOf = (number) => {
   const factors = [];
-  if (number > 1) {
-    factors.push(2);
+  let divisor = 2;
+  while (number > 1) {
+    for (; number % divisor === 0; number /= divisor) {factors.push(divisor);}
+    divisor++;
   }
   return factors;
 };
 
-describe.only('prime factors', () => {
+describe('prime factors', () => {
   it('return [none] for 1', () => {
     primeFactorsOf(1).should.deepEqual([]);
   });
+
   it('return [2] for 2', () => {
     primeFactorsOf(2).should.deepEqual([2]);
   });
-  it('return [3] for 3');
-  it('return [2, 2] for 4');
-  it('return [5] for 5');
-  it('return [2, 3] for 6');
-  it('return [7] for 7');
-  it('return [2, 2, 2] for 8');
-  it('return [3, 3] for 9');
+
+  it('return [3] for 3', () => {
+    primeFactorsOf(3).should.deepEqual([3]);
+  });
+
+  it('return [2, 2] for 4', () => {
+    primeFactorsOf(4).should.deepEqual([2, 2]);
+  });
+
+  it('return [5] for 5', () => {
+    primeFactorsOf(5).should.deepEqual([5]);
+  });
+
+  it('return [2, 3] for 6', () => {
+    primeFactorsOf(6).should.deepEqual([2, 3]);
+  });
+
+  it('return [7] for 7', () => {
+    primeFactorsOf(7).should.deepEqual([7]);
+  });
+
+  it('return [2, 2, 2] for 8', () => {
+    primeFactorsOf(8).should.deepEqual([2, 2, 2]);
+  });
+
+  it('return [3, 3] for 9', () => {
+    primeFactorsOf(9).should.deepEqual([3, 3]);
+  });
+
+  it('return factors of 2016', () => {
+    console.log(primeFactorsOf(2016));
+    primeFactorsOf(2106).should.deepEqual([2, 3, 3, 3, 3, 13]);
+  });
 });
