d965d8fcec2458d3ddb551b69862e836e1f338f6
D Greenstein <david.h.greenstein@gmail.com>
2017-08-03 13:39:49 -0500

    make fetch module and scaffold API

--- server/06-unusual-spending/fetch.js
+++ server/06-unusual-spending/fetch.js
@@ -1,3 +1,13 @@
-const fetch = () => {};
+import {prior, current} from './months';
+import {api} from './api';
+
+const fetch = (userId) => {
+  let priorMonth = prior();
+  let currentMonth = current();
+  let priorMonthPayments = api(userId, priorMonth);
+  let currentMonthPayments = api(userId, currentMonth);
+
+  return [priorMonthPayments, currentMonthPayments];
+};
 
 export {fetch};
